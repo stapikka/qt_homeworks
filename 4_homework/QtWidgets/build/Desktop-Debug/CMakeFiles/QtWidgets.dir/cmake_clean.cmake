2628b74df1ae5604e71563088eb8fbc028ba2612
Denis Balunin <balunya232000@gmai.com>
2026-02-01 13:54:38 +0300

    Add homework 4

---
+++ 4_homework/QtWidgets/build/Desktop-Debug/CMakeFiles/QtWidgets.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtWidgets_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QtWidgets_autogen.dir/ParseCache.txt"
+  "QtWidgets_autogen"
+  "CMakeFiles/QtWidgets.dir/QtWidgets_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/QtWidgets.dir/QtWidgets_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/QtWidgets.dir/main.cpp.o"
+  "CMakeFiles/QtWidgets.dir/main.cpp.o.d"
+  "CMakeFiles/QtWidgets.dir/mainwindow.cpp.o"
+  "CMakeFiles/QtWidgets.dir/mainwindow.cpp.o.d"
+  "QtWidgets"
+  "QtWidgets.pdb"
+  "QtWidgets_autogen/mocs_compilation.cpp"
+  "QtWidgets_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtWidgets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
